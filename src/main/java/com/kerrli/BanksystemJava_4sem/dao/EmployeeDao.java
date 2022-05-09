dfb16e6d56af57c9f2fe796a2e280ce496dc13d4
Anna Bogatyreva <87075158+AnnaABogatyreva@users.noreply.github.com>
2022-05-09 14:39:10 +0300

    Delete src directory

--- src/main/java/com/kerrli/BanksystemJava_4sem/dao/EmployeeDao.java
+++
@@ -1,7 +0,0 @@
-package com.kerrli.BanksystemJava_4sem.dao;
-
-import com.kerrli.BanksystemJava_4sem.entity.Employee;
-
-public interface EmployeeDao {
-    public Employee findByLogin(String login);
-}
